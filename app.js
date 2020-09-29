8f865473e66e1bee54b86b4302f665dc5a44c219
sofi-gal <61889647+sofi-gal@users.noreply.github.com>
2020-09-29 18:06:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,14 +11,18 @@ btns.forEach(function(btn) {
         const styles = e.currentTarget.classList;
         if(styles.contains('decrease')){
         	count--;
-        	value.style.color = 'red';
         }else if(styles.contains('increase')){
         	count++;
-        	value.style.color = 'green';
         } else{
         	count = 0;
-        	value.style.color = 'black';
         }
+		if(count > 0){
+			value.style.color = 'green';
+		}else if(count < 0){
+			value.style.color = 'red';
+		}else{
+			value.style.color = 'black';
+		}
   
         value.textContent = count;
 	});
